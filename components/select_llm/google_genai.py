7e04cb03b0829a6d45acd0121cf9df15737325f7
pro402 <maitypro402@gmail.com>
2025-04-21 12:52:25 +0530

    testing

---
+++ components/select_llm/google_genai.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+load_dotenv()
+google_api = os.getenv("GOOGLE_API")
+
+def set_llm(
+        model: str = "gemma-3-27b-it",
+):
+    """
+    Set the LLM to use based on user input.
+    Args:
+        model (str): The model to use. Default is "gemma-3-27b-it".
+        Choose model from https://aistudio.google.com
+    Returns:
+        llm: The selected LLM.
+    """
+    llm = ChatGoogleGenerativeAI(
+        model=model,
+        temperature=0,
+        max_tokens=8192,
+        api_key = google_api,
+    )
+    return llm
---
+++ components/select_llm/ollama.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from langchain_ollama.llms import OllamaLLM
+
+def set_llm(
+        model: str = "hf.co/mradermacher/Qwen2.5-0.5B-Instruct-GGUF:Q8_0",
+):
+    """
+    Set the LLM to use based on user input.
+    Args:
+        model (str): The model to use. Default is "hf.co/mradermacher/Qwen2.5-0.5B-Instruct-GGUF:Q8_0".
+        Choose model by typing ollama list in terminal.
+    Returns:
+        llm: The selected LLM.
+    """
+    llm = OllamaLLM(
+        model=model,
+        temperature=0,
+        seed=42,
+    )
+    return llm
