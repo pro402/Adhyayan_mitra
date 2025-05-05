a535d71912b2ee7a06fe255d9572360269f3da7f
Prasun <maitypro402@gmail.com>
2025-05-06 01:11:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ apt-get install -y portaudio19-dev python3-pyaudio
 
 # Verify installation
 ls -la /usr/lib/x86_64-linux-gnu/libportaudio*
+
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull hf.co/mradermacher/Qwen2.5-0.5B-Instruct-GGUF:Q8_0
